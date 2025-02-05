a4a63bc1e47ad4f0e5cb1106dd246abfd7dda6d9
pooolili <pooolili@proton.me>
2025-02-05 10:52:14 +0000

    1

--- packages/mtmai/mtmai/workflows/flow_ag.py
+++ packages/mtmai/mtmai/workflows/flow_ag.py
@@ -5,8 +5,6 @@ from typing import cast
 from agents.ctx import AgentContext
 from autogen_agentchat.messages import TextMessage
 from fastapi import HTTPException
-
-# from loguru import logger
 from mtmaisdk.clients.rest.models import PostizState
 from mtmaisdk.context.context import Context
 from pydantic import BaseModel
@@ -15,38 +13,39 @@ from mtmai.ag.team_builder import TeamBuilder
 from mtmai.agents.ctx import get_mtmai_context, init_mtmai_context
 from mtmai.worker import wfapp
 
-# from ..ag.team_builder import TeamBuilder
+# from loguru import logger
+
 
 logger = logging.getLogger(__name__)
 
 
 async def run_stream(task: str):
-    try:
-        team_builder = TeamBuilder()
-        # team = await team_builder.create_demo_team()
+    # try:
+    team_builder = TeamBuilder()
+    # team = await team_builder.create_demo_team()
 
-        agent = await team_builder.create_demo_agent_stream1()
-        # team_runner = TeamRunner()
+    agent = await team_builder.create_demo_agent_stream1()
+    # team_runner = TeamRunner()
 
-        # async for event in team_runner.run_stream(
-        async for event in agent.run_stream(
-            task=task,
-            # team_config=agent.dump_component()
-        ):
-            if isinstance(event, TextMessage):
-                yield f"2:{event.model_dump_json()}\n"
-            # elif isinstance(event, ToolCallRequestEvent):
-            #     yield f"0:{json.dumps(obj=jsonable_encoder(event.content))}\n"
-            # elif isinstance(event, TeamResult):
-            #     yield f"0:{json.dumps(obj=event.model_dump_json())}\n"
+    # async for event in team_runner.run_stream(
+    async for event in agent.run_stream(
+        task=task,
+        # team_config=agent.dump_component()
+    ):
+        if isinstance(event, TextMessage):
+            yield f"2:{event.model_dump_json()}\n"
+        # elif isinstance(event, ToolCallRequestEvent):
+        #     yield f"0:{json.dumps(obj=jsonable_encoder(event.content))}\n"
+        # elif isinstance(event, TeamResult):
+        #     yield f"0:{json.dumps(obj=event.model_dump_json())}\n"
 
-            elif isinstance(event, BaseModel):
-                yield f"2:{event.model_dump_json()}\n"
-            else:
-                yield f"2:{json.dumps(f'unknown event: {str(event)},type:{type(event)}')}\n"
-    except Exception as e:
-        logger.exception("Streaming error")
-        yield f"2:{json.dumps({'error': str(e)})}\n"
+        elif isinstance(event, BaseModel):
+            yield f"2:{event.model_dump_json()}\n"
+        else:
+            yield f"2:{json.dumps(f'unknown event: {str(event)},type:{type(event)}')}\n"
+    # except Exception as e:
+    #     logger.exception("Streaming error")
+    #     yield f"2:{json.dumps({'error': str(e)})}\n"
 
 
 @wfapp.workflow(
