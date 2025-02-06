a88211202bcebf46544189b9ba47579e5ab8f8d4
Segun Adebayo <joseshegs@gmail.com>
2025-02-06 01:19:53 +0000

    Auto update all changes By gomtm

--- packages/mtmai/mtmai/_version.py
+++ packages/mtmai/mtmai/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.82'
-__version_tuple__ = version_tuple = (0, 0, 82)
+__version__ = version = '0.0.84'
+__version_tuple__ = version_tuple = (0, 0, 84)
