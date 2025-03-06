51f4dc5a7d8964612d7c54d7a79eda5cd7159cfd
jancjksi <jancjksi@proton.me>
2025-03-06 02:33:13 +0000

    1

--- packages/mtmai/mtmai/teams/sys_team/demo_handoffs_team.py
+++ packages/mtmai/mtmai/teams/sys_team/demo_handoffs_team.py
@@ -1,7 +1,6 @@
 import asyncio
-import json
 import uuid
-from typing import Any, List, Mapping, Tuple
+from typing import Any, Mapping
 
 from autogen_agentchat.base import TerminationCondition
 from autogen_agentchat.messages import AgentEvent, ChatMessage
@@ -13,215 +12,23 @@ from autogen_core import (
     Component,
     DefaultSubscription,
     DefaultTopicId,
-    FunctionCall,
     MessageContext,
-    RoutedAgent,
     SingleThreadedAgentRuntime,
     TopicId,
     TypeSubscription,
-    message_handler,
     try_get_known_serializers_for_type,
 )
-from autogen_core.models import (
-    AssistantMessage,
-    ChatCompletionClient,
-    FunctionExecutionResult,
-    FunctionExecutionResultMessage,
-    SystemMessage,
-    UserMessage,
-)
-from autogen_core.tools import FunctionTool, Tool
+from autogen_core.models import SystemMessage
+from autogen_core.tools import FunctionTool
 from loguru import logger
 from mtmai.context.context_client import TenantClient
 from mtmai.context.ctx import get_chat_session_id_ctx
 from mtmai.mtmpb.ag_pb2 import AgentRunInput
 from mtmai.teams.base_team import MtBaseTeam
-from mtmai.teams.sys_team._types import AgentResponse, MyMessage, UserLogin, UserTask
+from mtmai.teams.sys_team._types import MyMessage, UserLogin
 from pydantic import BaseModel
 
-
-class AIAgent(RoutedAgent):
-    def __init__(
-        self,
-        description: str,
-        system_message: SystemMessage,
-        model_client: ChatCompletionClient,
-        tools: List[Tool],
-        delegate_tools: List[Tool],
-        agent_topic_type: str,
-        user_topic_type: str,
-    ) -> None:
-        super().__init__(description)
-        self._system_message = system_message
-        self._model_client = model_client
-        self._tools = dict([(tool.name, tool) for tool in tools])
-        self._tool_schema = [tool.schema for tool in tools]
-        self._delegate_tools = dict([(tool.name, tool) for tool in delegate_tools])
-        self._delegate_tool_schema = [tool.schema for tool in delegate_tools]
-        self._agent_topic_type = agent_topic_type
-        self._user_topic_type = user_topic_type
-
-    @message_handler
-    async def handle_task(self, message: UserTask, ctx: MessageContext) -> None:
-        # Send the task to the LLM.
-        llm_result = await self._model_client.create(
-            messages=[self._system_message] + message.context,
-            tools=self._tool_schema + self._delegate_tool_schema,
-            cancellation_token=ctx.cancellation_token,
-        )
-        print(f"{'-'*80}\n{self.id.type}:\n{llm_result.content}", flush=True)
-        # Process the LLM result.
-        while isinstance(llm_result.content, list) and all(
-            isinstance(m, FunctionCall) for m in llm_result.content
-        ):
-            tool_call_results: List[FunctionExecutionResult] = []
-            delegate_targets: List[Tuple[str, UserTask]] = []
-            # Process each function call.
-            for call in llm_result.content:
-                arguments = json.loads(call.arguments)
-                if call.name in self._tools:
-                    # Execute the tool directly.
-                    result = await self._tools[call.name].run_json(
-                        arguments, ctx.cancellation_token
-                    )
-                    result_as_str = self._tools[call.name].return_value_as_string(
-                        result
-                    )
-                    tool_call_results.append(
-                        FunctionExecutionResult(
-                            call_id=call.id,
-                            content=result_as_str,
-                            is_error=False,
-                            name=call.name,
-                        )
-                    )
-                elif call.name in self._delegate_tools:
-                    # Execute the tool to get the delegate agent's topic type.
-                    result = await self._delegate_tools[call.name].run_json(
-                        arguments, ctx.cancellation_token
-                    )
-                    topic_type = self._delegate_tools[call.name].return_value_as_string(
-                        result
-                    )
-                    # Create the context for the delegate agent, including the function call and the result.
-                    delegate_messages = list(message.context) + [
-                        AssistantMessage(content=[call], source=self.id.type),
-                        FunctionExecutionResultMessage(
-                            content=[
-                                FunctionExecutionResult(
-                                    call_id=call.id,
-                                    content=f"Transferred to {topic_type}. Adopt persona immediately.",
-                                    is_error=False,
-                                    name=call.name,
-                                )
-                            ]
-                        ),
-                    ]
-                    delegate_targets.append(
-                        (topic_type, UserTask(context=delegate_messages))
-                    )
-                else:
-                    raise ValueError(f"Unknown tool: {call.name}")
-            if len(delegate_targets) > 0:
-                # Delegate the task to other agents by publishing messages to the corresponding topics.
-                for topic_type, task in delegate_targets:
-                    print(
-                        f"{'-'*80}\n{self.id.type}:\nDelegating to {topic_type}",
-                        flush=True,
-                    )
-                    await self.publish_message(
-                        task, topic_id=TopicId(topic_type, source=self.id.key)
-                    )
-            if len(tool_call_results) > 0:
-                print(f"{'-'*80}\n{self.id.type}:\n{tool_call_results}", flush=True)
-                # Make another LLM call with the results.
-                message.context.extend(
-                    [
-                        AssistantMessage(
-                            content=llm_result.content, source=self.id.type
-                        ),
-                        FunctionExecutionResultMessage(content=tool_call_results),
-                    ]
-                )
-                llm_result = await self._model_client.create(
-                    messages=[self._system_message] + message.context,
-                    tools=self._tool_schema + self._delegate_tool_schema,
-                    cancellation_token=ctx.cancellation_token,
-                )
-                print(f"{'-'*80}\n{self.id.type}:\n{llm_result.content}", flush=True)
-            else:
-                # The task has been delegated, so we are done.
-                return
-        # The task has been completed, publish the final result.
-        assert isinstance(llm_result.content, str)
-        message.context.append(
-            AssistantMessage(content=llm_result.content, source=self.id.type)
-        )
-        await self.publish_message(
-            AgentResponse(
-                context=message.context, reply_to_topic_type=self._agent_topic_type
-            ),
-            topic_id=TopicId(self._user_topic_type, source=self.id.key),
-        )
-
-
-class HumanAgent(RoutedAgent):
-    def __init__(
-        self, description: str, agent_topic_type: str, user_topic_type: str
-    ) -> None:
-        super().__init__(description)
-        self._agent_topic_type = agent_topic_type
-        self._user_topic_type = user_topic_type
-
-    @message_handler
-    async def handle_user_task(self, message: UserTask, ctx: MessageContext) -> None:
-        human_input = input("Human agent input: ")
-        print(f"{'-'*80}\n{self.id.type}:\n{human_input}", flush=True)
-        message.context.append(
-            AssistantMessage(content=human_input, source=self.id.type)
-        )
-        await self.publish_message(
-            AgentResponse(
-                context=message.context, reply_to_topic_type=self._agent_topic_type
-            ),
-            topic_id=TopicId(self._user_topic_type, source=self.id.key),
-        )
-
-
-class UserAgent(RoutedAgent):
-    def __init__(
-        self, description: str, user_topic_type: str, agent_topic_type: str
-    ) -> None:
-        super().__init__(description)
-        self._user_topic_type = user_topic_type
-        self._agent_topic_type = agent_topic_type
-
-    @message_handler
-    async def handle_user_login(self, message: UserLogin, ctx: MessageContext) -> None:
-        print(f"{'-'*80}\nUser login, session ID: {self.id.key}.", flush=True)
-        # Get the user's initial input after login.
-        user_input = input("User: ")
-        print(f"{'-'*80}\n{self.id.type}:\n{user_input}")
-        await self.publish_message(
-            UserTask(context=[UserMessage(content=user_input, source="User")]),
-            topic_id=TopicId(self._agent_topic_type, source=self.id.key),
-        )
-
-    @message_handler
-    async def handle_task_result(
-        self, message: AgentResponse, ctx: MessageContext
-    ) -> None:
-        # Get the user's input after receiving a response from an agent.
-        user_input = input("User (type 'exit' to close the session): ")
-        print(f"{'-'*80}\n{self.id.type}:\n{user_input}", flush=True)
-        if user_input.strip().lower() == "exit":
-            print(f"{'-'*80}\nUser session ended, session ID: {self.id.key}.")
-            return
-        message.context.append(UserMessage(content=user_input, source="User"))
-        await self.publish_message(
-            UserTask(context=message.context),
-            topic_id=TopicId(message.reply_to_topic_type, source=self.id.key),
-        )
+from ._agents import AIAgent, HumanAgent, UserAgent
 
 
 def execute_order(product: str, price: int) -> str:
@@ -307,6 +114,27 @@ class DemoHandoffsTeamConfig(BaseModel):
     ...
 
 
+# Closure
+async def collect_output_messages(
+    _runtime: ClosureContext,
+    message: UserLogin,
+    ctx: MessageContext,
+) -> None:
+    """Collect output messages from the group chat."""
+    # if isinstance(message, GroupChatStart):
+    #     if message.messages is not None:
+    #         for msg in message.messages:
+    #             event_logger.info(msg)
+    #             await self._output_message_queue.put(msg)
+    # elif isinstance(message, GroupChatMessage):
+    #     event_logger.info(message.message)
+    #     await self._output_message_queue.put(message.message)
+    # elif isinstance(message, GroupChatTermination):
+    #     event_logger.info(message.message)
+    #     self._stop_reason = message.message.content
+    logger.info(f"收到消息: message: {message}")
+
+
 class DemoHandoffsTeam(MtBaseTeam, Component[DemoHandoffsTeamConfig]):
     component_type = "team"
 
@@ -341,11 +169,7 @@ class DemoHandoffsTeam(MtBaseTeam, Component[DemoHandoffsTeamConfig]):
         # TODO: The runtime should be created by a managed context.
         # Background exceptions must not be ignored as it results in non-surfaced exceptions and early team termination.
         self._runtime = SingleThreadedAgentRuntime(ignore_unhandled_exceptions=False)
-
-        # Flag to track if the group chat has been initialized.
         self._initialized = False
-
-        # Flag to track if the group chat is running.
         self._is_running = False
 
     async def _init(self, runtime: AgentRuntime | None = None) -> None:
@@ -483,26 +307,6 @@ class DemoHandoffsTeam(MtBaseTeam, Component[DemoHandoffsTeamConfig]):
             )
         )
 
-        # Closure
-        async def collect_output_messages(
-            _runtime: ClosureContext,
-            message: UserLogin,
-            ctx: MessageContext,
-        ) -> None:
-            """Collect output messages from the group chat."""
-            # if isinstance(message, GroupChatStart):
-            #     if message.messages is not None:
-            #         for msg in message.messages:
-            #             event_logger.info(msg)
-            #             await self._output_message_queue.put(msg)
-            # elif isinstance(message, GroupChatMessage):
-            #     event_logger.info(message.message)
-            #     await self._output_message_queue.put(message.message)
-            # elif isinstance(message, GroupChatTermination):
-            #     event_logger.info(message.message)
-            #     self._stop_reason = message.message.content
-            logger.info(f"收到消息: message: {message}")
-
         await ClosureAgent.register_closure(
             runtime=self._runtime,
             type="collector",
@@ -606,4 +410,7 @@ class DemoHandoffsTeam(MtBaseTeam, Component[DemoHandoffsTeamConfig]):
         #     # Indicate that the team is no longer running.
         #     self._is_running = False
 
+        pass
+
+    async def show_state(self) -> None:
         pass
