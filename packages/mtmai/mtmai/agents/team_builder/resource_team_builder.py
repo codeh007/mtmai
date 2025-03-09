f2c578ca89e7747f10f572909e1e19621211d9d4
kpa87ccj <kpa87ccj@proton.me>
2025-03-09 10:04:44 +0000

    1

---
+++ packages/mtmai/mtmai/agents/team_builder/resource_team_builder.py
@@ -0,0 +1,40 @@
+from loguru import logger
+from mtmai.agents.team_builder.instagram_team_builder import InstagramTeamBuilder
+from mtmai.context.context_client import TenantClient
+
+
+def percentage_change_tool(start: float, end: float) -> float:
+    return ((end - start) / start) * 100
+
+
+class ResourceTeamBuilder:
+    """资源管理团队"""
+
+    @property
+    def name(self):
+        return "resource_team"
+
+    @property
+    def description(self):
+        return "resource_team"
+
+    async def create_team(self, resource_id: str):
+        tenant_client = TenantClient()
+        tid = tenant_client.tenant_id
+        model_client = await tenant_client.ag.default_model_client(
+            tenant_client.tenant_id
+        )
+        resource_data = await tenant_client.ag.resource_api.resource_get(
+            tenant=tid,
+            resource=resource_id,
+        )
+        # platform_account_data = PlatformAccountData.model_validate(
+        #     platform_account.content
+        # )
+        logger.info(f"resource_data: {resource_data}")
+        # return team
+        if resource_data.type == "platform_account":
+            return await InstagramTeamBuilder().create_team(model_client)
+        raise ValueError(
+            f"cant create team for unsupported resource type: {resource_data.type}"
+        )
--- packages/mtmai/mtmai/flows/flow_instagram.py
+++ packages/mtmai/mtmai/flows/flow_instagram.py
@@ -1,7 +1,7 @@
 from mtmai.agents.cancel_token import MtCancelToken
+from mtmai.agents.team_builder.resource_team_builder import ResourceTeamBuilder
 from mtmai.clients.rest.models.instagram_task import InstagramTask
 from mtmai.context.context import Context
-from mtmai.teams.instagram_team import InstagramTeam
 from mtmai.worker_app import mtmapp
 
 
@@ -12,10 +12,11 @@ from mtmai.worker_app import mtmapp
 class FlowInstagram:
     @mtmapp.step(timeout="60m")
     async def step_entry(self, hatctx: Context):
-        instagram_agent = InstagramTeam()
-
-        result = await instagram_agent.run(
-            task=InstagramTask.model_validate(hatctx.input),
+        input = InstagramTask.model_validate(hatctx.input)
+        builder = ResourceTeamBuilder()
+        team = await builder.create_team(input.resource_id)
+        result = await team.run(
+            task=input.content,
             cancellation_token=MtCancelToken(),
         )
         return result
