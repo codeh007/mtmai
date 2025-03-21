dc26a1dd90fa13cfc3f0fe6c97a6487a5b9901f0
jksxzzid45 f <jksxzzid45@proton.me>
2025-03-21 11:12:16 +0000

    1

--- packages/mtmai/mtmai/worker_app.py
+++ packages/mtmai/mtmai/worker_app.py
@@ -1,14 +1,12 @@
-from autogen_core import try_get_known_serializers_for_type
 from loguru import logger
 
-from mtmai.agents.greeter_team import AskToGreet, Feedback, GreeterTeam, Greeting
-from mtmai.clients.rest.models.chat_session_start_event import ChatSessionStartEvent
+# from mtmai.agents.greeter_team import AskToGreet, Feedback, GreeterTeam, Greeting
 from mtmai.core.config import settings
 from mtmai.hatchet import Hatchet
 
 mtmapp = Hatchet()
 
-serializer_types = [ChatSessionStartEvent, AskToGreet, Greeting, Feedback]
+# serializer_types = [ChatSessionStartEvent, AskToGreet, Greeting, Feedback]
 
 
 def get_workflows_types():
@@ -27,15 +25,15 @@ async def run_worker():
     # from mtmai.flows.flow_dur import my_durable_func  # noqa
 
     worker = mtmapp.worker(settings.WORKER_NAME)
-    for serializer_type in serializer_types:
-        worker.agent_runtime.add_message_serializer(
-            try_get_known_serializers_for_type(serializer_type)
-        )
+    # for serializer_type in serializer_types:
+    #     worker.agent_runtime.add_message_serializer(
+    #         try_get_known_serializers_for_type(serializer_type)
+    #     )
     _start_coroutine = worker.agent_runtime.start()
     if _start_coroutine:
         await _start_coroutine
-    greeter_team = GreeterTeam()
-    await greeter_team.setup(worker.agent_runtime)
+    # greeter_team = GreeterTeam()
+    # await greeter_team.setup(worker.agent_runtime)
 
     # 另外一个持续运行的团队
     # team2 = DemoHandoffsTeam()
