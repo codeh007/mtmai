89ee478143184c0864c586b72a3a91b591a8ce95
jksxzzid45 f <jksxzzid45@proton.me>
2025-03-22 12:13:07 +0000

    1

--- packages/mtmai/mtmai/clients/ag.py
+++ packages/mtmai/mtmai/clients/ag.py
@@ -1,4 +1,11 @@
 from autogen_agentchat.base import Team
+from autogen_agentchat.conditions import MaxMessageTermination
+from autogen_agentchat.teams._group_chat._round_robin_group_chat import (
+    RoundRobinGroupChatConfig,
+)
+from autogen_core import ComponentModel
+from clients.rest.api.agent_api import AgentApi
+from clients.rest.api.team_api import TeamApi
 from connecpy.context import ClientContext
 from mtmai.clients.rest.api.ag_state_api import AgStateApi
 from mtmai.clients.rest.api.chat_api import ChatApi
@@ -84,6 +91,20 @@ class AgClient:
         self._resource_api = ResourceApi(self.api_client)
         return self._resource_api
 
+    @property
+    def team_api(self):
+        if hasattr(self, "_team_api"):
+            return self._team_api
+        self._team_api = TeamApi(self.api_client)
+        return self._team_api
+
+    @property
+    def agent_api(self):
+        if hasattr(self, "_agent_api"):
+            return self._agent_api
+        self._agent_api = AgentApi(self.api_client)
+        return self._agent_api
+
     async def load_team_state(
         self,
         chat_id: str,
@@ -131,3 +152,47 @@ class AgClient:
                 component=team_comp.model_dump(),
             ).model_dump(),
         )
+
+    async def get_team_v2(self, tenant_id: str, team_id: str):
+        agents_data = await self.agent_api.agent_list(
+            tenant=tenant_id,
+            team=team_id,
+        )
+
+        agent_components = [
+            ComponentModel(
+                provider=agent.provider,
+                component_type="agent",
+                label=agent.name,
+                description=agent.description,
+                config=agent.config,
+                version="1.0.0",
+                component_version="1.0.0",
+            )
+            for agent in agents_data
+        ]
+
+        team_data = await self.team_api.team_get(
+            tenant=tenant_id,
+            team=team_id,
+        )
+
+        max_messages_termination = MaxMessageTermination(max_messages=25)
+        termination = max_messages_termination
+
+        team_component = ComponentModel(
+            provider=team_data.provider,
+            component_type="team",
+            label=team_data.name,
+            description=team_data.description,
+            version="1.0.0",
+            component_version="1.0.0",
+            config=RoundRobinGroupChatConfig(
+                participants=agent_components,
+                termination_condition=termination,
+                max_turns=25,
+            ).model_dump(),
+        )
+        team = Team.load_component(team_component)
+
+        return team
