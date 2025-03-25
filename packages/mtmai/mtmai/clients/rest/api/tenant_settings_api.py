92fcb8c13959c5f9e68f61216f21fb8a894c366b
paicjka87d s <paicjka87d@proton.me>
2025-03-25 09:21:09 +0000

    1

--- packages/mtmai/mtmai/clients/rest/api/tenant_settings_api.py
+++ packages/mtmai/mtmai/clients/rest/api/tenant_settings_api.py
@@ -878,6 +878,8 @@ class TenantSettingsApi:
     async def tenant_settings_upsert(
         self,
         tenant: Annotated[str, Field(min_length=36, strict=True, max_length=36, description="The tenant id")],
+        setting: Annotated[StrictStr, Field(description="The setting id")],
+        tenant_setting_upsert: TenantSettingUpsert,
         _request_timeout: Union[
             None,
             Annotated[StrictFloat, Field(gt=0)],
@@ -896,6 +898,10 @@ class TenantSettingsApi:
 
         :param tenant: The tenant id (required)
         :type tenant: str
+        :param setting: The setting id (required)
+        :type setting: str
+        :param tenant_setting_upsert: (required)
+        :type tenant_setting_upsert: TenantSettingUpsert
         :param _request_timeout: timeout setting for this request. If one
                                  number provided, it will be total request
                                  timeout. It can also be a pair (tuple) of
@@ -920,6 +926,8 @@ class TenantSettingsApi:
 
         _param = self._tenant_settings_upsert_serialize(
             tenant=tenant,
+            setting=setting,
+            tenant_setting_upsert=tenant_setting_upsert,
             _request_auth=_request_auth,
             _content_type=_content_type,
             _headers=_headers,
@@ -929,7 +937,7 @@ class TenantSettingsApi:
         _response_types_map: Dict[str, Optional[str]] = {
             '200': "TenantSetting",
             '400': "APIErrors",
-            '403': "APIError",
+            '403': "APIErrors",
         }
         response_data = await self.api_client.call_api(
             *_param,
@@ -946,6 +954,8 @@ class TenantSettingsApi:
     async def tenant_settings_upsert_with_http_info(
         self,
         tenant: Annotated[str, Field(min_length=36, strict=True, max_length=36, description="The tenant id")],
+        setting: Annotated[StrictStr, Field(description="The setting id")],
+        tenant_setting_upsert: TenantSettingUpsert,
         _request_timeout: Union[
             None,
             Annotated[StrictFloat, Field(gt=0)],
@@ -964,6 +974,10 @@ class TenantSettingsApi:
 
         :param tenant: The tenant id (required)
         :type tenant: str
+        :param setting: The setting id (required)
+        :type setting: str
+        :param tenant_setting_upsert: (required)
+        :type tenant_setting_upsert: TenantSettingUpsert
         :param _request_timeout: timeout setting for this request. If one
                                  number provided, it will be total request
                                  timeout. It can also be a pair (tuple) of
@@ -988,6 +1002,8 @@ class TenantSettingsApi:
 
         _param = self._tenant_settings_upsert_serialize(
             tenant=tenant,
+            setting=setting,
+            tenant_setting_upsert=tenant_setting_upsert,
             _request_auth=_request_auth,
             _content_type=_content_type,
             _headers=_headers,
@@ -997,7 +1013,7 @@ class TenantSettingsApi:
         _response_types_map: Dict[str, Optional[str]] = {
             '200': "TenantSetting",
             '400': "APIErrors",
-            '403': "APIError",
+            '403': "APIErrors",
         }
         response_data = await self.api_client.call_api(
             *_param,
@@ -1014,6 +1030,8 @@ class TenantSettingsApi:
     async def tenant_settings_upsert_without_preload_content(
         self,
         tenant: Annotated[str, Field(min_length=36, strict=True, max_length=36, description="The tenant id")],
+        setting: Annotated[StrictStr, Field(description="The setting id")],
+        tenant_setting_upsert: TenantSettingUpsert,
         _request_timeout: Union[
             None,
             Annotated[StrictFloat, Field(gt=0)],
@@ -1032,6 +1050,10 @@ class TenantSettingsApi:
 
         :param tenant: The tenant id (required)
         :type tenant: str
+        :param setting: The setting id (required)
+        :type setting: str
+        :param tenant_setting_upsert: (required)
+        :type tenant_setting_upsert: TenantSettingUpsert
         :param _request_timeout: timeout setting for this request. If one
                                  number provided, it will be total request
                                  timeout. It can also be a pair (tuple) of
@@ -1056,6 +1078,8 @@ class TenantSettingsApi:
 
         _param = self._tenant_settings_upsert_serialize(
             tenant=tenant,
+            setting=setting,
+            tenant_setting_upsert=tenant_setting_upsert,
             _request_auth=_request_auth,
             _content_type=_content_type,
             _headers=_headers,
@@ -1065,7 +1089,7 @@ class TenantSettingsApi:
         _response_types_map: Dict[str, Optional[str]] = {
             '200': "TenantSetting",
             '400': "APIErrors",
-            '403': "APIError",
+            '403': "APIErrors",
         }
         response_data = await self.api_client.call_api(
             *_param,
@@ -1075,296 +1099,6 @@ class TenantSettingsApi:
 
 
     def _tenant_settings_upsert_serialize(
-        self,
-        tenant,
-        _request_auth,
-        _content_type,
-        _headers,
-        _host_index,
-    ) -> RequestSerialized:
-
-        _host = None
-
-        _collection_formats: Dict[str, str] = {
-        }
-
-        _path_params: Dict[str, str] = {}
-        _query_params: List[Tuple[str, str]] = []
-        _header_params: Dict[str, Optional[str]] = _headers or {}
-        _form_params: List[Tuple[str, str]] = []
-        _files: Dict[
-            str, Union[str, bytes, List[str], List[bytes], List[Tuple[str, bytes]]]
-        ] = {}
-        _body_params: Optional[bytes] = None
-
-        # process the path parameters
-        if tenant is not None:
-            _path_params['tenant'] = tenant
-        # process the query parameters
-        # process the header parameters
-        # process the form parameters
-        # process the body parameter
-
-
-        # set the HTTP header `Accept`
-        if 'Accept' not in _header_params:
-            _header_params['Accept'] = self.api_client.select_header_accept(
-                [
-                    'application/json'
-                ]
-            )
-
-
-        # authentication setting
-        _auth_settings: List[str] = [
-            'basicAuth', 
-            'cookieAuth', 
-            'bearerAuth'
-        ]
-
-        return self.api_client.param_serialize(
-            method='POST',
-            resource_path='/api/v1/tenants/{tenant}/settings',
-            path_params=_path_params,
-            query_params=_query_params,
-            header_params=_header_params,
-            body=_body_params,
-            post_params=_form_params,
-            files=_files,
-            auth_settings=_auth_settings,
-            collection_formats=_collection_formats,
-            _host=_host,
-            _request_auth=_request_auth
-        )
-
-
-
-
-    @validate_call
-    async def tenant_settings_upsert_0(
-        self,
-        tenant: Annotated[str, Field(min_length=36, strict=True, max_length=36, description="The tenant id")],
-        setting: Annotated[StrictStr, Field(description="The setting id")],
-        tenant_setting_upsert: Annotated[TenantSettingUpsert, Field(description="The model properties to update")],
-        _request_timeout: Union[
-            None,
-            Annotated[StrictFloat, Field(gt=0)],
-            Tuple[
-                Annotated[StrictFloat, Field(gt=0)],
-                Annotated[StrictFloat, Field(gt=0)]
-            ]
-        ] = None,
-        _request_auth: Optional[Dict[StrictStr, Any]] = None,
-        _content_type: Optional[StrictStr] = None,
-        _headers: Optional[Dict[StrictStr, Any]] = None,
-        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
-    ) -> TenantSetting:
-        """tenant_settings_upsert_0
-
-
-        :param tenant: The tenant id (required)
-        :type tenant: str
-        :param setting: The setting id (required)
-        :type setting: str
-        :param tenant_setting_upsert: The model properties to update (required)
-        :type tenant_setting_upsert: TenantSettingUpsert
-        :param _request_timeout: timeout setting for this request. If one
-                                 number provided, it will be total request
-                                 timeout. It can also be a pair (tuple) of
-                                 (connection, read) timeouts.
-        :type _request_timeout: int, tuple(int, int), optional
-        :param _request_auth: set to override the auth_settings for an a single
-                              request; this effectively ignores the
-                              authentication in the spec for a single request.
-        :type _request_auth: dict, optional
-        :param _content_type: force content-type for the request.
-        :type _content_type: str, Optional
-        :param _headers: set to override the headers for a single
-                         request; this effectively ignores the headers
-                         in the spec for a single request.
-        :type _headers: dict, optional
-        :param _host_index: set to override the host_index for a single
-                            request; this effectively ignores the host_index
-                            in the spec for a single request.
-        :type _host_index: int, optional
-        :return: Returns the result object.
-        """ # noqa: E501
-
-        _param = self._tenant_settings_upsert_0_serialize(
-            tenant=tenant,
-            setting=setting,
-            tenant_setting_upsert=tenant_setting_upsert,
-            _request_auth=_request_auth,
-            _content_type=_content_type,
-            _headers=_headers,
-            _host_index=_host_index
-        )
-
-        _response_types_map: Dict[str, Optional[str]] = {
-            '200': "TenantSetting",
-            '400': "APIErrors",
-            '403': "APIErrors",
-        }
-        response_data = await self.api_client.call_api(
-            *_param,
-            _request_timeout=_request_timeout
-        )
-        await response_data.read()
-        return self.api_client.response_deserialize(
-            response_data=response_data,
-            response_types_map=_response_types_map,
-        ).data
-
-
-    @validate_call
-    async def tenant_settings_upsert_0_with_http_info(
-        self,
-        tenant: Annotated[str, Field(min_length=36, strict=True, max_length=36, description="The tenant id")],
-        setting: Annotated[StrictStr, Field(description="The setting id")],
-        tenant_setting_upsert: Annotated[TenantSettingUpsert, Field(description="The model properties to update")],
-        _request_timeout: Union[
-            None,
-            Annotated[StrictFloat, Field(gt=0)],
-            Tuple[
-                Annotated[StrictFloat, Field(gt=0)],
-                Annotated[StrictFloat, Field(gt=0)]
-            ]
-        ] = None,
-        _request_auth: Optional[Dict[StrictStr, Any]] = None,
-        _content_type: Optional[StrictStr] = None,
-        _headers: Optional[Dict[StrictStr, Any]] = None,
-        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
-    ) -> ApiResponse[TenantSetting]:
-        """tenant_settings_upsert_0
-
-
-        :param tenant: The tenant id (required)
-        :type tenant: str
-        :param setting: The setting id (required)
-        :type setting: str
-        :param tenant_setting_upsert: The model properties to update (required)
-        :type tenant_setting_upsert: TenantSettingUpsert
-        :param _request_timeout: timeout setting for this request. If one
-                                 number provided, it will be total request
-                                 timeout. It can also be a pair (tuple) of
-                                 (connection, read) timeouts.
-        :type _request_timeout: int, tuple(int, int), optional
-        :param _request_auth: set to override the auth_settings for an a single
-                              request; this effectively ignores the
-                              authentication in the spec for a single request.
-        :type _request_auth: dict, optional
-        :param _content_type: force content-type for the request.
-        :type _content_type: str, Optional
-        :param _headers: set to override the headers for a single
-                         request; this effectively ignores the headers
-                         in the spec for a single request.
-        :type _headers: dict, optional
-        :param _host_index: set to override the host_index for a single
-                            request; this effectively ignores the host_index
-                            in the spec for a single request.
-        :type _host_index: int, optional
-        :return: Returns the result object.
-        """ # noqa: E501
-
-        _param = self._tenant_settings_upsert_0_serialize(
-            tenant=tenant,
-            setting=setting,
-            tenant_setting_upsert=tenant_setting_upsert,
-            _request_auth=_request_auth,
-            _content_type=_content_type,
-            _headers=_headers,
-            _host_index=_host_index
-        )
-
-        _response_types_map: Dict[str, Optional[str]] = {
-            '200': "TenantSetting",
-            '400': "APIErrors",
-            '403': "APIErrors",
-        }
-        response_data = await self.api_client.call_api(
-            *_param,
-            _request_timeout=_request_timeout
-        )
-        await response_data.read()
-        return self.api_client.response_deserialize(
-            response_data=response_data,
-            response_types_map=_response_types_map,
-        )
-
-
-    @validate_call
-    async def tenant_settings_upsert_0_without_preload_content(
-        self,
-        tenant: Annotated[str, Field(min_length=36, strict=True, max_length=36, description="The tenant id")],
-        setting: Annotated[StrictStr, Field(description="The setting id")],
-        tenant_setting_upsert: Annotated[TenantSettingUpsert, Field(description="The model properties to update")],
-        _request_timeout: Union[
-            None,
-            Annotated[StrictFloat, Field(gt=0)],
-            Tuple[
-                Annotated[StrictFloat, Field(gt=0)],
-                Annotated[StrictFloat, Field(gt=0)]
-            ]
-        ] = None,
-        _request_auth: Optional[Dict[StrictStr, Any]] = None,
-        _content_type: Optional[StrictStr] = None,
-        _headers: Optional[Dict[StrictStr, Any]] = None,
-        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
-    ) -> RESTResponseType:
-        """tenant_settings_upsert_0
-
-
-        :param tenant: The tenant id (required)
-        :type tenant: str
-        :param setting: The setting id (required)
-        :type setting: str
-        :param tenant_setting_upsert: The model properties to update (required)
-        :type tenant_setting_upsert: TenantSettingUpsert
-        :param _request_timeout: timeout setting for this request. If one
-                                 number provided, it will be total request
-                                 timeout. It can also be a pair (tuple) of
-                                 (connection, read) timeouts.
-        :type _request_timeout: int, tuple(int, int), optional
-        :param _request_auth: set to override the auth_settings for an a single
-                              request; this effectively ignores the
-                              authentication in the spec for a single request.
-        :type _request_auth: dict, optional
-        :param _content_type: force content-type for the request.
-        :type _content_type: str, Optional
-        :param _headers: set to override the headers for a single
-                         request; this effectively ignores the headers
-                         in the spec for a single request.
-        :type _headers: dict, optional
-        :param _host_index: set to override the host_index for a single
-                            request; this effectively ignores the host_index
-                            in the spec for a single request.
-        :type _host_index: int, optional
-        :return: Returns the result object.
-        """ # noqa: E501
-
-        _param = self._tenant_settings_upsert_0_serialize(
-            tenant=tenant,
-            setting=setting,
-            tenant_setting_upsert=tenant_setting_upsert,
-            _request_auth=_request_auth,
-            _content_type=_content_type,
-            _headers=_headers,
-            _host_index=_host_index
-        )
-
-        _response_types_map: Dict[str, Optional[str]] = {
-            '200': "TenantSetting",
-            '400': "APIErrors",
-            '403': "APIErrors",
-        }
-        response_data = await self.api_client.call_api(
-            *_param,
-            _request_timeout=_request_timeout
-        )
-        return response_data.response
-
-
-    def _tenant_settings_upsert_0_serialize(
         self,
         tenant,
         setting,
