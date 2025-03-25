116bf1ce03e2767b68b84c0d84e4510703fa96ed
paicjka87d s <paicjka87d@proton.me>
2025-03-25 04:19:06 +0000

    1

--- packages/mtmai/mtmai/flows/flow_ig.py
+++ packages/mtmai/mtmai/flows/flow_ig.py
@@ -16,119 +16,6 @@ IG_CREDENTIAL_PATH = "./ig_settings.json"
 SLEEP_TIME = "600"  # in seconds
 
 
-class Bot:
-    _cl = None
-
-    def __init__(self):
-        self._cl = Client()
-        if os.path.exists(IG_CREDENTIAL_PATH):
-            self._cl.load_settings(IG_CREDENTIAL_PATH)
-            self._cl.login(IG_USERNAME, IG_PASSWORD)
-        else:
-            self._cl.login(IG_USERNAME, IG_PASSWORD)
-            self._cl.dump_settings(IG_CREDENTIAL_PATH)
-
-    def follow_by_username(self, username) -> bool:
-        """
-        Follow a user
-
-        Parameters
-        ----------
-        username: str
-            Username for an Instagram account
-
-        Returns
-        -------
-        bool
-            A boolean value
-        """
-        userid = self._cl.user_id_from_username(username)
-        return self._cl.user_follow(userid)
-
-    def unfollow_by_username(self, username) -> bool:
-        """
-        Unfollow a user
-
-        Parameters
-        ----------
-        username: str
-            Username for an Instagram account
-
-        Returns
-        -------
-        bool
-            A boolean value
-        """
-        userid = self._cl.user_id_from_username(username)
-        return self._cl.user_unfollow(userid)
-
-    def get_followers(self, amount: int = 0) -> Dict[int, UserShort]:
-        """
-        Get bot's followers
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        Dict[int, UserShort]
-            Dict of user_id and User object
-        """
-        return self._cl.user_followers(self._cl.user_id, amount=amount)
-
-    def get_followers_usernames(self, amount: int = 0) -> List[str]:
-        """
-        Get bot's followers usernames
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        List[str]
-            List of usernames
-        """
-        followers = self._cl.user_followers(self._cl.user_id, amount=amount)
-        return [user.username for user in followers.values()]
-
-    def get_following(self, amount: int = 0) -> Dict[int, UserShort]:
-        """
-        Get bot's followed users
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        Dict[int, UserShort]
-            Dict of user_id and User object
-        """
-        return self._cl.user_following(self._cl.user_id, amount=amount)
-
-    def get_following_usernames(self, amount: int = 0) -> List[str]:
-        """
-        Get bot's followed usernames
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        List[str]
-            List of usernames
-        """
-        following = self._cl.user_following(self._cl.user_id, amount=amount)
-        return [user.username for user in following.values()]
-
-
 @mtmapp.workflow(
     name="ig",
     on_events=["ig"],
@@ -137,111 +24,27 @@ class FlowIG:
     _cl = None
 
     def __init__(self):
-        self._cl = Client()
-        if os.path.exists(IG_CREDENTIAL_PATH):
-            self._cl.load_settings(IG_CREDENTIAL_PATH)
-            self._cl.login(IG_USERNAME, IG_PASSWORD)
-        else:
-            self._cl.login(IG_USERNAME, IG_PASSWORD)
-            self._cl.dump_settings(IG_CREDENTIAL_PATH)
+        pass
 
     def follow_by_username(self, username) -> bool:
-        """
-        Follow a user
-
-        Parameters
-        ----------
-        username: str
-            Username for an Instagram account
-
-        Returns
-        -------
-        bool
-            A boolean value
-        """
         userid = self._cl.user_id_from_username(username)
         return self._cl.user_follow(userid)
 
     def unfollow_by_username(self, username) -> bool:
-        """
-        Unfollow a user
-
-        Parameters
-        ----------
-        username: str
-            Username for an Instagram account
-
-        Returns
-        -------
-        bool
-            A boolean value
-        """
         userid = self._cl.user_id_from_username(username)
         return self._cl.user_unfollow(userid)
 
     def get_followers(self, amount: int = 0) -> Dict[int, UserShort]:
-        """
-        Get bot's followers
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        Dict[int, UserShort]
-            Dict of user_id and User object
-        """
         return self._cl.user_followers(self._cl.user_id, amount=amount)
 
     def get_followers_usernames(self, amount: int = 0) -> List[str]:
-        """
-        Get bot's followers usernames
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        List[str]
-            List of usernames
-        """
         followers = self._cl.user_followers(self._cl.user_id, amount=amount)
         return [user.username for user in followers.values()]
 
     def get_following(self, amount: int = 0) -> Dict[int, UserShort]:
-        """
-        Get bot's followed users
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        Dict[int, UserShort]
-            Dict of user_id and User object
-        """
         return self._cl.user_following(self._cl.user_id, amount=amount)
 
     def get_following_usernames(self, amount: int = 0) -> List[str]:
-        """
-        Get bot's followed usernames
-
-        Parameters
-        ----------
-        amount: int, optional
-            Maximum number of media to return, default is 0 - Inf
-
-        Returns
-        -------
-        List[str]
-            List of usernames
-        """
         following = self._cl.user_following(self._cl.user_id, amount=amount)
         return [user.username for user in following.values()]
 
@@ -252,6 +55,16 @@ class FlowIG:
         tenant_client = TenantClient()
         session_id = get_chat_session_id_ctx()
         tid = get_tenant_id()
+
+        ig_client = Client()
+
+        if os.path.exists(IG_CREDENTIAL_PATH):
+            ig_client.load_settings(IG_CREDENTIAL_PATH)
+            ig_client.login(IG_USERNAME, IG_PASSWORD)
+        else:
+            ig_client.login(IG_USERNAME, IG_PASSWORD)
+            ig_client.dump_settings(IG_CREDENTIAL_PATH)
+
         return {
             "status": "browser ok, success",
         }
