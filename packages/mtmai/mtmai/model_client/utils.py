b8c4d3b96c2bc326d53dabb0d8aec7125fb5afc2
alb988192 s <alb988192@proton.me>
2025-03-28 02:15:34 +0000

    1

--- packages/mtmai/mtmai/model_client/utils.py
+++ packages/mtmai/mtmai/model_client/utils.py
@@ -1,5 +1,3 @@
-import os
-
 from autogen_core.models import ModelFamily, ModelInfo
 
 from mtmai import tools as tools
@@ -23,7 +21,7 @@ def get_default_model_client():
 
 
 def get_custom_model():
-    from huggingface_hub import InferenceClient, login
+    from huggingface_hub import login
     from smolagents import OpenAIServerModel
 
     login(settings.HF_TOKEN)
@@ -31,22 +29,21 @@ def get_custom_model():
     # model_id = "meta-llama/Llama-3.3-70B-Instruct"
     # client = InferenceClient(model=model_id)
     open_ai_client = OpenAIServerModel(
-        model_id="gpt-4o",
-        api_base="https://api.openai.com/v1",
-        api_key=os.environ["OPENAI_API_KEY"],
+        model_id="nvidia/llama-3.3-nemotron-super-49b-v1",
+        api_base="https://integrate.api.nvidia.com/v1",
+        api_key=settings.NVIDIA_API_KEY,
     )
 
-    fal_ai_client = InferenceClient(
-        provider="fal-ai",
-        api_key="df5c6ec4-7b6e-4640-b1a0-a4bf0a89a554:d57cb77650115512fd5069240a339119",
-    )
+    # fal_ai_client = InferenceClient(
+    #     provider="fal-ai",
+    #     api_key="df5c6ec4-7b6e-4640-b1a0-a4bf0a89a554:d57cb77650115512fd5069240a339119",
+    # )
 
-    def custom_model(messages, stop_sequences=["Task"]):
-        response = fal_ai_client.chat_completion(
-            messages, stop=stop_sequences, max_tokens=1000
-        )
-        answer = response.choices[0].message
-        return answer
+    # def custom_model(messages, stop_sequences=["Task"]):
+    #     response = fal_ai_client.chat_completion(
+    #         messages, stop=stop_sequences, max_tokens=1000
+    #     )
+    #     answer = response.choices[0].message
+    #     return answer
 
     return open_ai_client
-    # return custom_model
